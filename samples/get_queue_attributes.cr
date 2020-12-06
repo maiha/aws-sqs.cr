d0e4142da8e1bdf3b1342fa271b2098932568630
maiha <maiha@wota.jp>
2020-12-06 18:50:58 +0900

    samples: get_queue_attributes.cr now accepts attribute_names

--- samples/get_queue_attributes.cr
+++ samples/get_queue_attributes.cr
@@ -1,14 +1,16 @@
 require "../src/aws-sqs"
 
 USAGE = <<-EOF
-  usage: get_queue_attributes.cr <QUEUE_URL> [<MESSAGE_BODY>]
+  usage: get_queue_attributes.cr <QUEUE_URL> [<ATTRIBUTE_NAME>...]
     ENV["AWS_DEFAULT_REGION"]    : #{ENV["AWS_DEFAULT_REGION"]? || "(not found)"}
     ENV["AWS_ACCESS_KEY_ID"]     : #{ENV["AWS_ACCESS_KEY_ID"]? ? "OK" : "(not found)"}
     ENV["AWS_SECRET_ACCESS_KEY"] : #{ENV["AWS_SECRET_ACCESS_KEY"]? ? "OK" : "(not found)"}
 
-    example)
-      crystal samples/get_queue_attributes.cr "https://sqs.us-east-2.amazonaws.com/123456789012/MyQueue"
+    ATTRIBUTE_NAME
+      #{Aws::SQS::Types::QueueAttributeName.names.join(", ")}
 
+    example)
+      crystal samples/get_queue_attributes.cr "https://sqs.us-east-2.amazonaws.com/123456789012/MyQueue" "ApproximateNumberOfMessages"
   EOF
 
 # arg1: queue_url
@@ -19,6 +21,11 @@ if queue_url
   end
 end
 
+attribute_names = ARGV
+if attribute_names.empty?
+  attribute_names = ["QueueArn", "ApproximateNumberOfMessages", "MaximumMessageSize"]
+end    
+
 if queue_url 
   region = ENV["AWS_DEFAULT_REGION"]
   access = ENV["AWS_ACCESS_KEY_ID"]
@@ -26,7 +33,7 @@ if queue_url
 
   client = Aws::SQS::Client.new(region: region, aws_access_key: access, aws_secret_key: secret)
 
-  res = client.get_queue_attributes(queue_url: queue_url, attribute_names: ["QueueArn", "ApproximateNumberOfMessages"])
+  res = client.get_queue_attributes(queue_url: queue_url, attribute_names: attribute_names)
   p res
 else
   puts USAGE
